7dfbe1e904883710c3919861576f7e730eba813f
Lali <lauradiaz1586@gmail.com>
2021-01-07 18:04:51 +0400

    work on all projects page, gallery layout style and logic

--- pages/all/index.js
+++ pages/all/index.js
@@ -1,27 +1,55 @@
-// import Link from 'next/link'
+import Link from 'next/link'
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 
 export default function All({ data }) {
   const projects = data
+  const videoMp4 = data.filter((elem) => elem.id === '31')
+  const extractVideo = videoMp4[0].mp4Video
 
   return (
-    <div className="imageContainer">
-      <div className="col">
-        {projects.map((project, key) => (
-          <img
-            className="imagen"
-            key={project.id}
-            src={project.image}
-            alt={project.name}
-          />
-        ))}
-      </div>
+    <div className="wrapperAll">
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+        <Masonry columnsCount={3} gutter={4}>
+          {projects.map((project, key) => (
+            <Link
+              key={project.id}
+              href={`/projects/[slug]`}
+              as={`/projects/${project.slug}`}
+            >
+              <a>
+                <div className="containerAll">
+                  {project.id == 31 ? (
+                    <video
+                      autoPlay
+                      muted
+                      loop
+                      width="100%"
+                      height="auto"
+                      className="videoClass"
+                    >
+                      <source src={extractVideo} type="video/mp4" />
+                    </video>
+                  ) : (
+                    <img
+                      className="imagen"
+                      alt={project.name}
+                      src={project.image}
+                    />
+                  )}
+                  <div className="middleAll">
+                    <p className="textAll">{project.name}</p>
+                  </div>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
 
       <style jsx>{`
-        .imageContainer {
-          max-width: 100vw;
-          height: auto;
-          padding: 40px 10px 0px 10px;
-          column-count: 3;
+        .wrapperAll {
+          width: 100%;
+          padding: 40px 0px 0px 0px;
         }
 
         @media screen and (max-width: 667px) {
@@ -29,14 +57,45 @@ export default function All({ data }) {
             column-count: 1;
           }
         }
-        .col {
-          height: auto;
+
+        .containerAll {
+          position: relative;
+        }
+        .containerAll:hover img {
+          opacity: 0.5;
+          filter: grayscale(100%);
         }
 
+        .containerAll:hover .videoClass {
+          opacity: 0.5;
+          filter: grayscale(100%);
+        }
+        .containerAll:hover .middleAll {
+          opacity: 1;
+        }
         .imagen {
           width: 100%;
-          border-radius: 10px;
-          padding: 10px;
+          padding-left: 10px;
+          padding-bottom: 5px;
+          width: 100%;
+          height: 100%;
+        }
+        .middleAll {
+          padding: 20px;
+          position: absolute;
+          bottom: 0;
+          right: 0;
+          width: 100%;
+          opacity: 0;
+          text-align: center;
+          font-weight: bold;
+          transition: ease 0.5s all;
+        }
+
+        .textAll {
+          font-size: 20px;
+          color: white;
+          padding: 5px 10px 25px 10px;
         }
       `}</style>
     </div>
